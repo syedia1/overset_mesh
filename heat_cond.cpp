9a441d556e18828fb67828accaaa6ab903b76457
syedia1 <74667219+syedia1@users.noreply.github.com>
2024-05-28 16:48:52 +0530

    one way mesh transfer
    code for one-way background mesh to component mesh information transfer.
    
    plot file now calculates l2 norm of error from analytical results for 2d plate.
    
    error folder has different mesh results for order of error calculation.

--- heat_cond.cpp
+++ heat_cond.cpp
@@ -1,44 +1,253 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include <iterator>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
+typedef vector<vector<pair<vector<size_t>, vector<double> > > > vec3id;
 typedef vector<vector<double> > vec2d;
+typedef vector<pair<vector<size_t>, vector<double> > > vec2id;
+typedef vector<vector<size_t> > vec2i;
 typedef vector<double> vec1d;
+typedef vector<size_t> vec1i;
+
+const double PI = 3.141592653589793;
+const double TOLERANCE = 1024 * std::numeric_limits<double>::epsilon();
+// const double TOLERANCE = 1e-6;
+const size_t NS_MAXITER = 20;
+
+
+// function declarations
+void BC(vec2d &T, const vec1d T_b);
+void meshTransfer(const vec2d &T, vec2d &T2, const vec3id &interpolation_pts);
+void gridSolve(vec2d &Tn1, const vec2d &Tn, const double del_t, const double alpha, const vec1d del_mesh);
+double rmsT(const vec2d &Tn, const vec2d &Tn1);
+vec1d mesh2to1(const vec1d mesh2_origin, const vec1d del_mesh2, const size_t i, const size_t j);
+vec3id interpolation_pts(const vec2d &T, const vec2d &T2, const vec1d del_mesh, const vec1d del_mesh2, const vec1d mesh2_origin);
+vec1d interpolation_coeff(const vec2d points, const vec1d xy);
+
+vec2d matMul(const vec2d &A, const vec2d &B);
+vec1d matAdd(const vec1d &A, const vec1d &B);
+vec1d solveAxB(const vec2d &A, const vec1d &B);
+
 
 void vecToText(const vec2d& T, string filename) {
   // saving results to file
   std::ofstream output_file(filename);
   size_t Ny = T.size()-1-2, Nx = T[0].size()-1-2; // 1 based indexing, 2 boundary cells
-  for(size_t i = 2; i <= Nx+1; i++){
-    for(size_t j = 2; j <= Ny+1; j++){
-			output_file<<T[j][i]<<" ";
+  for(size_t j = 2; j <= Ny+1; j++){
+    for(size_t i = 2; i <= Nx+1; i++){
+			output_file << std::fixed << std::setprecision(std::numeric_limits<double>::digits10+2) << T[j][i]<<" ";
     }
-		output_file<<endl;
+		output_file << endl;
   }
 }
 
-void meshTransfer(const vec2d &T, vec2d &T2, const size_t x2, const size_t y2) {
+vec2d matMul(const vec2d &A, const vec2d &B) {
+  size_t m = A.size(), n = B[0].size(), r = A[0].size();
+  vec2d C (m, vec1d(n, 0.0));
+  // order of j and k loops is swapped for contigous access of C and B, cache friendly
+  for (size_t i = 0 ; i < m; i++) {
+    for (size_t k = 0 ; k < r; k++) {
+      for (size_t j = 0 ; j < n; j++) {
+        C[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+  return C;
+}
+
+vec1d matAdd(const vec1d &A, const vec1d &B) {
+  size_t m = A.size();
+  vec1d C (m, 0.0);
+  for (size_t i = 0 ; i < m; i++) {
+    C[i] = A[i] + B[i];
+  }
+  return C;
+}
+
+vec1d solveAxB(const vec2d &A, const vec1d &B) {
+  size_t n = A.size(); 
+  vec1d x(n, 0.0);
+
+  // Pre-calculate inverses of diagonal elements (avoid division within the loop)
+  vec1d inv_diag(n, 0.0);
+  for (size_t i = 0; i < n; ++i) {
+    inv_diag[i] = 1.0 / A[i][i];
+  }
+
+  // Loop for iterations, Guass Seidel
+  size_t GS_itr = 0;
+  for (; GS_itr < NS_MAXITER; ++GS_itr) {
+
+    // Update each element of the solution vector
+    double sum = 0.0;
+    bool converged = true;
+    for (size_t j = 0; j < n; ++j) {
+      sum = 0.0;
+      for (size_t k = 0; k < n; ++k) {
+        if (j != k) {
+          sum += A[j][k] * x[k];
+        }
+      }
+      x[j] = (B[j] - sum) * inv_diag[j];
+      converged &= (abs(x[j]*A[j][j] + sum - B[j]) <= TOLERANCE);
+    }
+
+    if (converged) {
+      break;
+    }
+  }
+  // cout << "GS=" << GS_itr << ", ";
+  return x;
+}
+
+vec1d interpolation_coeff(const vec2d points, const vec1d xy){
+  auto transform = [](const vec1d &coeff, const vec1d &points) -> double {
+    const double phi = coeff[0], chi = coeff[1];
+    return (1.0-phi)*(1.0-chi) * points[0] + (phi)*(1.0-chi) * points[1] + (phi)*(chi) * points[2] + (1.0-phi)*(chi) * points[3];
+  };
+  auto partialDerivative_phi = [](const vec1d &coeff, const vec1d &points) {
+    const double chi = coeff[1];
+    return (-1.0)*(1.0-chi) * points[0] + (1.0-chi) * points[1] + (chi) * points[2] + (-1.0)*(chi) * points[3];  
+  };
+  auto partialDerivative_chi = [](const vec1d &coeff, const vec1d &points) {
+    const double phi = coeff[0];
+    return (1.0-phi)*(-1.0) * points[0] + (phi) * (-1.0) * points[1] + (phi) * points[2] + (1.0-phi) * points[3];
+  };
+
+  vec1d coeff = {0.5, 0.5}; // initial guess value of phi,
+  
+  // NR, Newton raphson to minimze B, Ax = B
+  size_t NR_itr = 0;
+  for (; NR_itr < NS_MAXITER; NR_itr++) {
+    vec2d A = {{partialDerivative_phi(coeff, points[0]), partialDerivative_chi(coeff, points[0])}, 
+      {partialDerivative_phi(coeff, points[1]), partialDerivative_chi(coeff, points[1])}};
+    vec1d B = {-1.0*transform(coeff, points[0])+xy[0], -1.0*transform(coeff, points[1])+xy[1]};
+    
+    coeff = matAdd(coeff, solveAxB(A, B));
+    // check for convergence
+    bool converged = true;
+    for (int eq = 0; eq < 2; ++eq) {
+      converged &= (abs(B[eq]) <= TOLERANCE);
+    }
+    if (converged) {
+      break;
+    }
+  }
+  // cout << "NR=" << NR_itr << ", ";
+  return vec1d {(1.0-coeff[0])*(1.0-coeff[1]), (coeff[0])*(1.0-coeff[1]), (coeff[0])*(coeff[1]), (1.0-coeff[0])*(coeff[1])};
+}
+
+vec1d mesh2to1(const vec1d mesh2_origin, const vec1d del_mesh2, const size_t i, const size_t j){
+  // coordinate transforms between 2 cartesian grids shifted by x2, y2 and rotated counter-clock by theta
+  vec1d xy(2, 0.0);
+  const double x2 = mesh2_origin[0], y2 = mesh2_origin[1];
+  const double sinTheta = mesh2_origin[2], cosTheta = mesh2_origin[3];
+  const double del_x2 = del_mesh2[0], del_y2 = del_mesh2[1];
+  xy[0] = x2 + ((i-2.0)*del_x2*cosTheta - (j-2.0)*del_y2*sinTheta);
+  xy[1] = y2 + ((i-2.0)*del_x2*sinTheta + (j-2.0)*del_y2*cosTheta);
+  return xy;
+} 
+
+vec3id interpolation_pts(const vec2d &T, const vec2d &T2, const vec1d del_mesh, const vec1d del_mesh2, const vec1d mesh2_origin){
+  // 3d vector of interpolation points for boundaries of mesh 2 counter clockwise from bottom edge
+  // innermost vector has i, j i.e. first interpolation donor cells of mesh 1 and coeffs i.e phi and chi 
+
+  // size_t Ny = T.size()-1-2, Nx = T[0].size()-1-2; // 1 based indexing, 2 boundary cells  
+  size_t Ny2 = T2.size()-1-2, Nx2 = T2[0].size()-1-2; // 1 based indexing, 2 boundary cells
+  vec3id inter_points;
+  vec2id temp_line;
+  //bottom edge
+  for (size_t i = 2; i <= Nx2+1; i++) {
+    vec1d xy = mesh2to1(mesh2_origin, del_mesh2, i, 2);
+    size_t _i = floor(xy[0]/del_mesh[0]);
+    size_t _j = floor(xy[1]/del_mesh[1]);
+    vec1d coeff = interpolation_coeff(vec2d {{_i * del_mesh[0], (_i+1)*del_mesh[0], (_i+1) * del_mesh[0], _i*del_mesh[0]}, {_j*del_mesh[1], _j*del_mesh[1], (_j+1)*del_mesh[1], (_j+1)*del_mesh[1]}}, xy);
+    temp_line.push_back(make_pair(vec1i {_i+2, _j+2}, coeff));
+  } 
+  inter_points.push_back(temp_line);
+  temp_line.clear();
+
+  //right edge
+  for (size_t j = 2; j <= Ny2+1; j++) {
+    vec1d xy = mesh2to1(mesh2_origin, del_mesh2, Nx2+1, j);
+    size_t _i = floor(xy[0]/del_mesh[0]);
+    size_t _j = floor(xy[1]/del_mesh[1]);
+    vec1d coeff = interpolation_coeff(vec2d {{_i * del_mesh[0], (_i+1)*del_mesh[0], (_i+1) * del_mesh[0], (_i)*del_mesh[0]}, {_j*del_mesh[1], _j*del_mesh[1], (_j+1)*del_mesh[1], (_j+1)*del_mesh[1]}}, xy);
+    temp_line.push_back(make_pair(vec1i {_i+2, _j+2}, coeff));
+  } 
+  inter_points.push_back(temp_line);
+  temp_line.clear();
+
+  //top edge
+  for (size_t i = 2; i <= Nx2+1; i++) {
+    vec1d xy = mesh2to1(mesh2_origin, del_mesh2, i, Ny2+1);
+    size_t _i = floor(xy[0]/del_mesh[0]);
+    size_t _j = floor(xy[1]/del_mesh[1]);
+    vec1d coeff = interpolation_coeff(vec2d {{_i * del_mesh[0], (_i+1)*del_mesh[0], (_i+1) * del_mesh[0], (_i)*del_mesh[0]}, {_j*del_mesh[1], _j*del_mesh[1], (_j+1)*del_mesh[1], (_j+1)*del_mesh[1]}}, xy);
+    temp_line.push_back(make_pair(vec1i {_i+2, _j+2}, coeff));
+  } 
+  inter_points.push_back(temp_line);
+  temp_line.clear();
+
+
+  //left edge
+  for (size_t j = 2; j <= Ny2+1; j++) {
+    vec1d xy = mesh2to1(mesh2_origin, del_mesh2, 2, j);
+    size_t _i = floor(xy[0]/del_mesh[0]);
+    size_t _j = floor(xy[1]/del_mesh[1]);
+    vec1d coeff = interpolation_coeff(vec2d {{_i * del_mesh[0], (_i+1)*del_mesh[0], (_i+1) * del_mesh[0], (_i)*del_mesh[0]}, {_j*del_mesh[1], _j*del_mesh[1], (_j+1)*del_mesh[1], (_j+1)*del_mesh[1]}}, xy);
+    temp_line.push_back(make_pair(vec1i {_i+2, _j+2}, coeff));
+  } 
+  inter_points.push_back(temp_line);
+  return inter_points;
+}
+
+void meshTransfer(const vec2d &T, vec2d &T2, const vec3id &interpolation_pts) {
   // size_t Ny = T.size()-1-2, Nx = T[0].size()-1-2; // 1 based indexing, 2 boundary cells
   size_t Ny2 = T2.size()-1-2, Nx2 = T2[0].size()-1-2; // 1 based indexing, 2 boundary cells
   
+  size_t bIdx = 0;
   // bottom edge
   for (size_t i = 2; i <= Nx2+1; i++) {
-    T2[1][i] =  T[2+y2-1][i+x2];
+    size_t _i = interpolation_pts[bIdx][i-2].first[0], _j = interpolation_pts[bIdx][i-2].first[1];
+    vec2i pts = {{_i, _j}, {_i+1, _j}, {_i, _j+1}, {_i+1, _j+1}};
+    T2[1][i] = 0.0;
+    for (size_t idx = 0; idx < 4; idx++){
+      T2[1][i] += T[pts[idx][1]][pts[idx][0]] * interpolation_pts[bIdx][i-2].second[idx];
+    }
   } 
+  bIdx = 1;
   // right edge
   for (size_t j = 2; j <= Ny2+1; j++) {
-    T2[j][Nx2+2] =  T[j+y2][2+x2+Nx2];
+    size_t _i = interpolation_pts[bIdx][j-2].first[0], _j = interpolation_pts[bIdx][j-2].first[1];
+    vec2i pts = {{_i, _j}, {_i+1, _j}, {_i, _j+1}, {_i+1, _j+1}};
+    T2[j][Nx2+2] = 0.0;
+    for (size_t idx = 0; idx < 4; idx++){
+      T2[j][Nx2+2] += T[pts[idx][1]][pts[idx][0]] * interpolation_pts[bIdx][j-2].second[idx];
+    }
   } 
+  bIdx = 2;
   // top edge
   for (size_t i = 2; i <= Nx2+1; i++) {
-    T2[Ny2+2][i] =  T[2+y2+Ny2][i+x2];
+    size_t _i = interpolation_pts[bIdx][i-2].first[0], _j = interpolation_pts[bIdx][i-2].first[1];
+    vec2i pts = {{_i, _j}, {_i+1, _j}, {_i, _j+1}, {_i+1, _j+1}};
+    T2[Ny2+2][i] = 0.0;
+    for (size_t idx = 0; idx < 4; idx++){
+      T2[Ny2+2][i] += T[pts[idx][1]][pts[idx][0]] * interpolation_pts[bIdx][i-2].second[idx];
+    }
   } 
+  bIdx = 3;
   // left edge
   for (size_t j = 2; j <= Ny2+1; j++) {
-    T2[j][1] =  T[j+y2][2+x2-1];
+    size_t _i = interpolation_pts[bIdx][j-2].first[0], _j = interpolation_pts[bIdx][j-2].first[1];
+    vec2i pts = {{_i, _j}, {_i+1, _j}, {_i, _j+1}, {_i+1, _j+1}};
+    T2[j][1] = 0.0;
+    for (size_t idx = 0; idx < 4; idx++){
+      T2[j][1] += T[pts[idx][1]][pts[idx][0]] * interpolation_pts[bIdx][j-2].second[idx];
+    }
   } 
 }
 
@@ -64,11 +273,11 @@ void BC(vec2d &T, const vec1d T_b) {
   } 
 }
 
-void gridSolve(vec2d &Tn1, const vec2d &Tn, const double del_t, const double alpha, const double del_x, const double del_y){
+void gridSolve(vec2d &Tn1, const vec2d &Tn, const double del_t, const double alpha, const vec1d del_mesh){
   size_t Ny = Tn.size()-1-2, Nx = Tn[0].size()-1-2; // 1 based indexing, 2 boundary cells
   for (size_t i = 2; i <= Nx+1; i++) {
     for (size_t j = 2; j <= Ny+1; j++) {
-      Tn1[j][i] = Tn[j][i] + del_t * alpha * ((Tn[j-1][i] - 2 * Tn[j][i] + Tn[j+1][i])/pow(del_y, 2) + (Tn[j][i-1] - 2 * Tn[j][i] + Tn[j][i+1])/pow(del_x, 2));
+      Tn1[j][i] = Tn[j][i] + del_t * alpha * ((Tn[j-1][i] - 2 * Tn[j][i] + Tn[j+1][i])/pow(del_mesh[1], 2) + (Tn[j][i-1] - 2 * Tn[j][i] + Tn[j][i+1])/pow(del_mesh[0], 2));
     }
   }
 }
@@ -87,60 +296,61 @@ double rmsT(const vec2d &Tn, const vec2d &Tn1) {
 
 void solver() {
   // Grid discretization AND Domain 
-  size_t x2 = 1, y2 = 1;
-  size_t Nx = 100, Ny = 100;
-  size_t Nx2 = 40, Ny2 = 98;
-  double L = 1, W = 1;
-  double L2 = L * Nx2/Nx, W2 = W * Ny2/Ny;
+  const size_t Nx = 160, Ny = 160;
+  const double L = 5.0, W = 5.0;
 
-  double del_x = L / Nx, del_y = W / Ny;
-  double del_x2 = L2 / Nx2, del_y2 = W2 / Ny2;
- 
-  // constant
-  double k = 16.2;
-  double rho = 7750;
-  double cp = 500.0;
-  double alpha = k / (rho * cp);
-  double del_t = (1 / (2 * alpha * (1/pow(del_x, 2) + 1/pow(del_y,2)))) * 0.9; // taking 90 % of the upper limit as per von Neumann stability analysis
-  double del_t2 = (1 / (2 * alpha * (1/pow(del_x2, 2) + 1/pow(del_y2,2)))) * 0.9; // taking 90 % of the upper limit as per von Neumann stability analysis
+  const double x2 = L/3, y2 = W/3, theta = -25.0 * PI/180.0;
+  const size_t Nx2 = 48, Ny2 = 48;
+  const double L2 = 1.5, W2 = 1.5;
+
+  const double del_x = L / Nx, del_y = W / Ny;
+  const double del_x2 = L2 / Nx2, del_y2 = W2 / Ny2;
+  const double cosTheta = cos(theta), sinTheta = sin(theta);
+  
+  const vec1d mesh2_origin = {x2, y2, sinTheta, cosTheta};
+  const vec1d del_mesh = {del_x, del_y};
+  const vec1d del_mesh2 = {del_x2, del_y2};
+
+  // constants
+  const double k = 16.2;
+  const double rho = 7750;
+  const double cp = 500.0;
+  const double alpha = k / (rho * cp);
+  const double del_t = (1 / (2 * alpha * (1/pow(del_x, 2) + 1/pow(del_y,2)))) * 0.9; // taking 90 % of the upper limit as per von Neumann stability analysis
+  const double del_t2 = (1 / (2 * alpha * (1/pow(del_x2, 2) + 1/pow(del_y2,2)))) * 0.9; 
   
   // 1 based indexing and 2 boundary cells
-	vec2d Tn(Ny+2 + 1, vec1d(Nx+2 + 1, 0));
-	vec2d Tn1(Ny+2 + 1, vec1d(Nx+2 + 1, 0));
+	vec2d Tn(Ny+2 + 1, vec1d(Nx+2 + 1, 0.0));
+	vec2d Tn1(Ny+2 + 1, vec1d(Nx+2 + 1, 0.0));
 
-	vec2d T2n(Ny2+2 + 1, vec1d(Nx2+2 + 1, 0));
-	vec2d T2n1(Ny2+2 + 1, vec1d(Nx2+2 + 1, 0));
+	vec2d T2n(Ny2+2 + 1, vec1d(Nx2+2 + 1, 0.0));
+	vec2d T2n1(Ny2+2 + 1, vec1d(Nx2+2 + 1, 0.0));
 
   // boundary condition values; counter clockwise from bottom edge
-  const vec1d T_bound = {100.0, 200.0, 200.0, 200.0};
-  void BC(vec2d &T, const vec1d T_b);
+  const vec1d T_bound = {200.0, 200.0, 100.0, 200.0};
 
-  // function declarations
-  void meshTransfer(const vec2d &T, vec2d &T2, const size_t x2, const size_t y2);
-  void gridSolve(vec2d &Tn1, const vec2d &Tn, const double del_t, const double alpha, const double del_x, const double del_y);
-  double rmsT(const vec2d &Tn, const vec2d &Tn1);
+  vec3id interpolated_pts = interpolation_pts(Tn, T2n, del_mesh, del_mesh2, mesh2_origin);
 
   BC(Tn, T_bound);
-  meshTransfer(Tn, T2n, x2, y2);
+  meshTransfer(Tn1, T2n1, interpolated_pts);
 
   // convergence constants
   double rms_T = 1.0;
   double rms_T2 = 1.0;
-  const double convergence_factor = 1e-5;
 
   int n = 0;
-  while (rms_T > convergence_factor || rms_T2 > convergence_factor){
-    gridSolve(Tn1, Tn, del_t, alpha, del_x, del_y);
+  while (rms_T > TOLERANCE || rms_T2 > TOLERANCE){
+    gridSolve(Tn1, Tn, del_t, alpha, del_mesh);
     BC(Tn1, T_bound);
-    meshTransfer(Tn1, T2n, x2, y2);
-    gridSolve(T2n1, T2n, del_t2, alpha, del_x2, del_y2);
+    meshTransfer(Tn1, T2n1, interpolated_pts);
+    gridSolve(T2n1, T2n, del_t2, alpha, del_mesh2);
 
     rms_T = rmsT(Tn1, Tn);
     rms_T2 = rmsT(T2n1, T2n);
     n += 1;
     Tn = Tn1;
     T2n = T2n1;
-    if (n % 1000 == 0){
+    if (n % 5000 == 0){
       cout << "Iterations = " << n << " RMS_T = " <<  rms_T << " RMS_T2 = " <<  rms_T2 << "\n";
     }
   }
--- plot.py
+++ plot.py
@@ -1,38 +1,137 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-mesh_no = "2"
-for mesh_no in ["1", "2"]:
-    # Load data
-    Tn1 = np.loadtxt("mesh"+mesh_no+".txt", dtype=float) 
+DTYPE = np.float64
 
-    L = 1
-    W = 1
-    Nx, Ny = Tn1.shape
-    Nx = Nx - 3
-    Ny = Ny - 3
-    var_max = 200
-    var_min = 100
+PI = np.pi
+l = [5, 1.5]
+w = [5, 1.5]
+mulFact = 8
+nx = np.array([20, 6])*mulFact
+ny = np.array([20, 6])*mulFact
+x0 = [0, l[0]/3]
+y0 = [0, w[0]/3]
+theta = np.array([0, -25]) * PI/180.0 # counter-clock rotation
+colours = ["k", "r", "b"]
+var_max = 200
+var_min = 100
+format = "%0.1f"
+N_levels = 20
 
-    # Plotting temperature
-    domain_specs = [Nx, Ny, L, W]
-    def temp_contour(domain_specs, field_var, title, filename, x_label = '$x$', y_label = '$y$'):
-        field_var = np.array(field_var).T
+print("Mesh discretization ", nx, ny, theta * 180.0/PI)
+# shift x0, y0 origins to cell center 
+for i in range(len(x0)):
+    dx = l[i]/nx[i]/2 # dx/2 i.e. L/Nx/2 is added as we have values at cell center
+    dy = w[i]/ny[i]/2
+    x0[i] = x0[i] + dx*np.cos(theta[i]) - dy*np.sin(theta[i])
+    y0[i] = y0[i] + dx*np.sin(theta[i]) + dy*np.cos(theta[i])
+
+def temp_contour(title, filename, x_label = '$x$', y_label = '$y$'):
+    # fig = plt.figure(figsize=(6 + 3, 6 * l[0]/w[0] + 2))
+    # Create figure and axes
+    fig, ax = plt.subplots(figsize=(6 + 3, 6 * l[0]/w[0] + 2))
+    ax.tick_params(left=False, bottom=False, labelbottom=False, labelleft=False)
+    for idx, mesh_no in enumerate(["1", "2"]):
+        # Load data
+        Tn1 = np.loadtxt("mesh"+mesh_no+".txt", dtype=DTYPE) 
+
+        L = l[idx]
+        W = l[idx]
+        Nx = nx[idx]
+        Ny = ny[idx]
+
+        # Plotting temperature
+        field_var = Tn1
+        # field_var = np.array(field_var).T
         # field_var = np.flip(field_var, axis=0)
-        Nx, Ny, L, W = domain_specs
-        xGrid, yGrid = np.meshgrid(np.linspace(0, L, Nx), np.linspace(0, W, Ny))
-        # print(xGrid.shape, yGrid.shape, field_var.shape)
+        xAxis = np.linspace(0, L, Nx) 
+        yAxis = np.linspace(0, W, Ny)
+        _xGrid, _yGrid = np.meshgrid(xAxis, yAxis)
+        xGrid = x0[idx] + _xGrid * np.cos(theta[idx]) - _yGrid * np.sin(theta[idx])
+        yGrid = y0[idx] + _xGrid * np.sin(theta[idx]) + _yGrid * np.cos(theta[idx])
+
         # Plotting field variable
-        format = "%0.1f"
-
-        plt.figure(figsize=(Nx//10 + 3, Ny//10 + 2))
-        plt.tricontourf(xGrid.flatten(), yGrid.flatten(), field_var.flatten(), levels = np.linspace(var_min, var_max, 11, endpoint=True))
-        plt.colorbar(ticks = np.linspace(var_min, var_max, 11, endpoint=True), format = format)
-        plt.title(title)
-        plt.xlabel(x_label)
-        plt.ylabel(y_label)
+        LEVELS = np.linspace(var_min, var_max, N_levels, endpoint=True)
+        contour = plt.tricontour(xGrid.flatten(), yGrid.flatten(), field_var.flatten(), levels = LEVELS, colors=colours[idx])
+        plt.clabel(contour, inline = 1)
+        ax.add_artist(plt.Rectangle((x0[idx], y0[idx]), L, W, angle = theta[idx]*180/PI,linewidth = 2, facecolor = "none", edgecolor=colours[idx]))
+        # plt.xlabel(x_label)
+        # plt.ylabel(y_label)
         # plt.show()
-        plt.savefig(filename)
-        plt.close()
+    # plt.colorbar(ticks = np.linspace(var_min, var_max, N_levels, endpoint=True), format = format)
+    
+    # ax.set_xlabel('100 C')
+    # fig.supylabel('200 C', x=0.92, y=0.5)
+    # fig.supxlabel('200 C', x=0.5, y=0.90)
+    # ax.set_ylabel('200 C')
+    plt.savefig(filename)
+    fig.suptitle(title)
+    plt.close()
 
-    temp_contour(domain_specs, Tn1[2:-1, 2:-1], "Temp contour for 2d plate mesh "+mesh_no, "temp_mesh"+mesh_no)
+temp_contour("Temp contour for 2d plate", "temp_overset") 
+# # temp_contour(Tn1, "Temp contour for 2d plate mesh "+mesh_no, "temp_mesh"+mesh_no)
+
+def analytical_sol(xGrid, yGrid, Nx, Ny):
+    T2, T1 = 100.0, 200.0
+    L = l[0] + l[0]/nx[0] 
+    W = w[0] + w[0]/ny[0]
+
+    T_ana = np.zeros((Ny, Nx))
+    for j in range(0, Ny):
+        for i in range(0, Nx):
+            x = xGrid[j][i]
+            y = yGrid[j][i]
+            fact = 0.0
+            for n in range(1, 300):
+                fact = fact + (pow(-1,n+1) + 1)/n * np.sin(n*PI*x/L) * np.sinh(n*PI*y/L) /np.sinh(n*PI*W/L)
+            T_ana[j][i] = T1 + (T2-T1) * 2/PI * fact
+
+    # return np.flip(T_ana)
+    return T_ana
+
+def error():
+
+    # fig, ax = plt.subplots(figsize=(6 + 3, 6 * l[0]/w[0] + 2))
+    for idx, mesh_no in enumerate(["1", "2"]):
+        # Load data
+        Tn1 = np.loadtxt("mesh"+mesh_no+".txt", dtype=DTYPE) 
+        L = l[idx]
+        W = l[idx]
+        Nx = nx[idx]
+        Ny = ny[idx]
+
+        # xAxis = np.linspace(0, L, Nx)+L/Nx/2 # dx/2 i.e. L/Nx/2 is added as we have values at cell center
+        # yAxis = np.linspace(0, W, Ny)+W/Ny/2
+        xAxis = np.linspace(0, L, Nx, dtype=DTYPE)
+        yAxis = np.linspace(0, W, Ny, dtype=DTYPE)
+        _xGrid, _yGrid = np.meshgrid(xAxis, yAxis)
+        xGrid = x0[idx] + _xGrid * np.cos(theta[idx]) - _yGrid * np.sin(theta[idx])
+        yGrid = y0[idx] + _xGrid * np.sin(theta[idx]) + _yGrid * np.cos(theta[idx])
+
+        T_analytical = analytical_sol(xGrid, yGrid, Nx, Ny)
+
+        error = np.linalg.norm(Tn1 - T_analytical, 2)
+        print("Mesh "+mesh_no+" l2 error =  ", error)
+
+        fig, ax = plt.subplots(figsize=(6 + 3, 6 * l[idx]/w[idx] + 2))
+        LEVELS = np.linspace(var_min, var_max, N_levels, endpoint=True)
+        
+        contour1 = plt.tricontour(xGrid.flatten(), yGrid.flatten(), T_analytical.flatten(), levels = LEVELS, colors=colours[0])
+        plt.clabel(contour1, inline = 1)
+        ax.add_artist(plt.Rectangle((x0[idx], y0[idx]), L, W, angle = theta[idx]*180/PI,linewidth = 2, facecolor = "none", edgecolor=colours[idx]))
+        
+        contour2 = plt.tricontour(xGrid.flatten(), yGrid.flatten(), Tn1.flatten(), levels = LEVELS, colors=colours[1])
+        plt.clabel(contour2, inline = 1)
+        # plt.show()
+        plt.savefig("Temp analytical_"+mesh_no)
+        plt.close()
+    # plt.savefig("Temp analytical_")
+    # plt.close()
+        
+        # plt.figure(figsize=(6 + 3, 6 * l[idx]/w[idx] + 2))
+        # plt.scatter(xGrid, yGrid)
+        # plt.savefig("scatter_"+mesh_no)
+        # # plt.show()
+        # plt.close()
+
+error()
