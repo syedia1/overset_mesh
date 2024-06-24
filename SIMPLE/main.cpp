4ff596aa7d14f2483b7d58f0c4ebbf113d4fb1af
syedia1 <74667219+syedia1@users.noreply.github.com>
2024-06-25 01:17:02 +0530

    Update raw C array to vector
    Replace raw C arrays (using multiple pointers) to C++ vectors.
    Simplified copying of old values to Variables to new arrays.
    Custom printing of RMs and iteration count.

--- SIMPLE/main.cpp
+++ SIMPLE/main.cpp
@@ -3,7 +3,12 @@
 #include <string>
 #include <math.h>
 
+#include <chrono>
+
 using namespace std;
+using namespace std::chrono;
+
+const size_t ITER_PRINT_FREQ = 10000;
 
 class DataStructure {
 public: 
@@ -11,11 +16,11 @@ public:
     double dx, dy, Lx, Ly;
     double dt;
     int nVar;
-    double ***Var;
-    double ***VarOld;
-    double ***Ff;
-    double *Init;
-    double *residual;
+    vector<vector<vector<double> > > Var;
+    vector<vector<vector<double> > > VarOld;
+    vector<vector<vector<double> > > Ff;
+    vector<double> Init;
+    vector<double> residual;
     double ulid;
     int Dim;
     double volp;
@@ -25,15 +30,7 @@ public:
 
 void CopyNewtoOld(DataStructure *rect)
 {
-    for(int k=0; k<rect->nVar; k++){
-      for(int i = 0; i<rect->Nx+2; i++)
-       {
-           for(int j = 0; j<rect->Ny+2; j++)
-                {
-                     rect->VarOld[k][i][j] = rect->Var[k][i][j];
-                }
-       }
-    }
+  std::copy(rect->Var.begin(), rect->Var.end(), rect->VarOld.begin());
 }
 
 void allocate(DataStructure *rect)
@@ -44,41 +41,42 @@ void allocate(DataStructure *rect)
    rect->Ly = 1.0;
    rect->dx = (rect->Lx/rect->Nx);
    rect->dy = (rect->Ly/rect->Ny);
-   rect->dt =0.001;//0.5*(rect->dx*rect->dx * rect->dy*rect->dy)/(rect->alpha * (rect->dx*rect->dx + rect->dy*rect->dy));
+   rect->dt = 0.001;//0.5*(rect->dx*rect->dx * rect->dy*rect->dy)/(rect->alpha * (rect->dx*rect->dx + rect->dy*rect->dy));
 
    rect->nVar = 3;
-   rect->Init = new double[rect->nVar];
+   rect->Init.resize(rect->nVar, 0.0);
+   rect->Var.resize(rect->nVar);
+   rect->VarOld.resize(rect->nVar);
    rect->ulid = 1.0;
-   rect->Var = new double**[rect->nVar];
-   rect->VarOld = new double**[rect->nVar];
    rect->Dim = 2;
    rect->volp = rect->dx*rect->dy;
    rect->nu = 0.01;
    rect->rho = 1.0;
+
    for(int i = 0; i<rect->nVar; i++)
    {
-       rect->Init[i] = 0.0;
-       rect->Var[i] =  new double*[rect->Nx+2];
-       rect->VarOld[i] =  new double*[rect->Nx+2];
+        rect->Var[i].resize(rect->Nx+2);
+        rect->VarOld[i].resize(rect->Nx+2);
        for(int j = 0; j<(rect->Nx+2); j++)
        {
-            rect->Var[i][j]=new double[rect->Ny+2];
-            rect->VarOld[i][j]=new double[rect->Ny+2];
+          rect->Var[i][j].resize(rect->Ny+2, 0.0);
+          rect->VarOld[i][j].resize(rect->Ny+2, 0.0);
        }
    }
-   rect->residual = new double[rect->nVar];
-   rect->Ff = new double**[2*rect->Dim]; //No of faces per cell = 4  i=0,1,2,3 -> E,N,W,S
+  rect->residual.resize(rect->nVar);
+  rect->Ff.resize(2*rect->Dim); //No of faces per cell = 4  i=0,1,2,3 -> E,N,W,S
    for(int k = 0; k<(2*rect->Dim); k++)
    {
-       rect->Ff[k] = new double*[rect->Nx+2];
+      rect->Ff[k].resize(rect->Nx+2);
            for(int j = 0; j<(rect->Nx+2); j++)
              {
-                rect->Ff[k][j]=new double[rect->Ny+2];
+                rect->Ff[k][j].resize(rect->Ny+2, 0.0);
              }
    }
    
    
 }
+
 void ApplyBC(DataStructure *rect, int k)
 {
     switch (k)
@@ -183,16 +181,7 @@ void GetOutput(DataStructure *rect, std::string input)
     }
     fclose(fp);
 } 
-void ExplicitSolve(DataStructure *rect)
-{ 
-   rect->residual = 0;
-   for(int i = 1; i<rect->Nx+1; i++)
-   {
-       for(int j = 1; j<rect->Ny+1; j++)
-            {
-            }
-   }
-}
+
 void SimpleUpwind(DataStructure *rect, double *Fc, double *ap_c, int i, int j, int k)
 {
     double ue, uw, un, us;   
@@ -229,7 +218,6 @@ void SimpleUpwind(DataStructure *rect, double *Fc, double *ap_c, int i, int j, i
     *Fc = (ue*rect->Ff[0][i][j] + uw*rect->Ff[2][i][j]  + un*rect->Ff[1][i][j]  +us*rect->Ff[3][i][j] );
     *ap_c = sum*rect->volp;
 }
-
 
 void Quick(DataStructure *rect, double *Fc, double *ap_c, int i, int j, int k)
 { 
@@ -278,7 +266,6 @@ void Quick(DataStructure *rect, double *Fc, double *ap_c, int i, int j, int k)
     *Fc = (ue*rect->Ff[0][i][j] + uw*rect->Ff[2][i][j]  + un*rect->Ff[1][i][j]  +us*rect->Ff[3][i][j] );
     *ap_c = sum*rect->volp;
 }
-
 
 void DiffusiveFlux(DataStructure *rect, double *Fd, double *ap_d, int i, int j, int k)
 {
@@ -375,16 +362,20 @@ void ImplicitSolve(DataStructure *rect)
     UpdateFlux(rect);     
 }
 
-bool ConvergenceCheck(DataStructure *rect)
+bool ConvergenceCheck(DataStructure *rect, int count)
 {
    double rms[rect->nVar];
    for(int k=0; k<rect->nVar; k++)
    {
-   rms[k] = sqrt(rect->residual[k]/(rect->Nx*rect->Ny));
+        rms[k] = sqrt(rect->residual[k]/(rect->Nx*rect->Ny));
         rms[k] = rms[k]/rect->dt;
-        cout << "\t" << rms[k];
+        if (count % ITER_PRINT_FREQ == 0) {
+          cout << "\t" << rms[k];
+        }
    }
-   cout<<endl;
+   if (count % ITER_PRINT_FREQ == 0) {
+    cout<<endl;
+   }
    if(rms[0] > 1e-12 || rms[1] > 1e-6 || rms[2] > 1e-6)
    {
        CopyNewtoOld(rect);
@@ -396,18 +387,19 @@ bool ConvergenceCheck(DataStructure *rect)
    }
    
 }
+
 void Solve(DataStructure *rect)
 {
     int count =0;
     do
     {
-        /* code */
         count++;
         ImplicitSolve(rect);
-       // ExplicitSolve(rect);
-      //  ApplyBC(rect,);
-        cout<<count;
-    }while (!ConvergenceCheck(rect));
+      if (count % ITER_PRINT_FREQ == 0){
+        cout << " Iter: " << count;
+      }
+    }while (!ConvergenceCheck(rect, count));
+    cout << "Solved in " << count << " iterations."<<endl;
 }
 
 int main()
@@ -416,6 +408,6 @@ int main()
     allocate(&rect);
     initialize(&rect);
     Solve(&rect);
-    GetOutput(&rect, "output_Quick.dat");
+    GetOutput(&rect, "output_Quick_test.dat");
     return 0;
 }
